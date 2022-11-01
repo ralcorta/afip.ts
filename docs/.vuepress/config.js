11291fc39d33c840a4727000fad2f69d0c94d2a4
Rodrigo <rodrigovictoralcorta@gmail.com>
2022-11-01 01:52:07 -0300

    test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 const { description } = require('../../package')
 
 module.exports = {
-  base: 'https://valiulab.github.io/afip.ts'
+  base: 'https://valiulab.github.io/afip.ts',
 
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
