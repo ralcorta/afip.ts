4a2d77ab62b10d03eb36f73de15961861392e088
Rodrigo Alcorta <rodrigovictoralcorta@gmail.com>
2023-09-09 20:00:56 -0300

    Edited: test config file is simpler

--- tests/utils/config.ts
+++ tests/utils/config.ts
@@ -2,22 +2,14 @@ import { Env } from "./../../src/utils/env";
 import { resolve } from "path";
 import { readFileSync } from "fs";
 
-export const testConfig = {
-  testCredentialsFolder: Env.testCredentialsFolder,
-  privateKeyFileName: Env.testPrivateKeyFileName,
-  certFileName: Env.testCertFileName,
-};
-
 export const testConfigUtil = {
   getTestCredentialsFolder: () => {
-    return testConfig.testCredentialsFolder;
+    return Env.testCredentialsFolder;
   },
   getKeyPath: () =>
-    resolve(
-      `/${testConfig.testCredentialsFolder}/${testConfig.privateKeyFileName}`
-    ),
+    resolve(`/${Env.testCredentialsFolder}/${Env.testPrivateKeyFileName}`),
   getCertPath: () =>
-    resolve(`/${testConfig.testCredentialsFolder}/${testConfig.certFileName}`),
+    resolve(`/${Env.testCredentialsFolder}/${Env.testCertFileName}`),
   getKey: () => readFileSync(testConfigUtil.getKeyPath(), { encoding: "utf8" }),
   getCert: () =>
     readFileSync(testConfigUtil.getCertPath(), { encoding: "utf8" }),
